e0664e40a1e1f8b7c7ea6cc69f613b703fc246a4
Sebastian Smiley <sebastianswms@gmail.com>
2024-10-19 13:29:33 -0400

    empty node

--- project/snowplow_ws/src/snowplow_pkg/setup.py
+++ project/snowplow_ws/src/snowplow_pkg/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'lateral_longitudinal_control_node = snowplow_pkg.lateral_longitudinal_control_node:main'
         ],
     },
 )
