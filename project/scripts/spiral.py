fd39d4b86fac44b49bff5a05b3484844e47e4d2a
Jon <jon@School2.myguest.virtualbox.org>
2024-11-05 20:45:15 -0500

    what I have so far, spiral function is still acting funky

--- project/scripts/spiral.py
+++ project/scripts/spiral.py
@@ -1,77 +1,155 @@
+import matplotlib.pyplot as plt # used for graphically showing the spiral 
+
+FIRST_CORNER = (0, 0)
+SECOND_CORNER = (10, 20)
+SNOWPLOW_WIDTH = 4
+
+
 """Gemerates waypoints based on a rectangle defined by its corners."""
 def generate_waypoints(second_corner):
     waypoints = []
     for i in range(second_corner[0] + 1):
         for j in range(second_corner[1] + 1): # plus one so we get 0 to the very back of the rectangle
-            waypoints.append((i, j))
+            waypoints.append((j, i))
     return waypoints
 
 """finds the exact width of the rectangle"""
-def get_width(first_corner, second_corner, Spwidth):
+def get_width(first_corner, second_corner):
     width = second_corner[0] - first_corner[0]
     return(round(width))
 
 """finds the exact height of the rectangle"""
-def get_height(first_corner, second_corner, Spwidth):
+def get_height(first_corner, second_corner):
     height = second_corner[1] - first_corner[1]
     return(round(height))
 
-"""this function may still need some work"""
-"""finds the exact center of the rectangle"""
-def get_center(waypoints):
-    center = round(len(waypoints)/2)
-    return waypoints[center] # returns the tuple that is at the center of the waypoints list
+"""finds center of snowplow"""
+def get_spcenter(snowplow_width):
+    spcenter = snowplow_width / 2
+    return round(spcenter)
 
 """function to build the spiral"""
-def spiral(TlCorner, Brcorner, rectwidth, rectheight):
-    # Initialize current position
-    currpoint = TlCorner
-    spirallist = []
-    
-    # Initialize direction offsets (right, down, left, up)
-    current_direction = 0  # start moving right
 
-    # Initialize bounds for spiraling
-    left_bound = 0
-    right_bound = rectwidth
-    top_bound = 0
-    bottom_bound = rectheight
+"""function to build the spiral"""
+def spiral(TlCorner, rectwidth, rectheight, spcenter):
+    spirallist = []  # List to hold every point in the spiral
+    current_direction = 0  # Start moving right
+
+    left_bound = 0 + spcenter  # Used to see how far left the spiral can go
+    right_bound = rectwidth - spcenter  # Used to see how far right the spiral can go
+    top_bound = 0 + spcenter  # Used to see how far up the spiral can go
+    bottom_bound = rectheight - spcenter  # Used to see how far down the spiral can go
     
-    # Counter for spiral filling
-    count = 0  # Count how many steps are taken
+    # All static values are used for moving to the edge of the spiral 
+    Static_lbound = 0
+    Static_rbound = rectwidth
+    Static_tbound = 0
+    Static_bbound = rectheight
+
+    for i in range(0, left_bound): # adds the extra points to get into the spiral
+        print("startingpath")
+        spirallist.append((left_bound, i))
 
     while left_bound <= right_bound and top_bound <= bottom_bound:
+        backup = []
         if current_direction == 0:  # Moving right
             for i in range(left_bound, right_bound + 1):
+                if i == right_bound:
+                    turn = (TlCorner[0] + i, TlCorner[1] + top_bound)
                 spirallist.append((TlCorner[0] + i, TlCorner[1] + top_bound))
-                count += 1
-            top_bound += 1
+            
+            # Go to the right edge and back
+            for i in range(right_bound + 1, Static_rbound + 1):
+                spirallist.append((TlCorner[0] + i, TlCorner[1] + top_bound))
+                backup.append((TlCorner[0] + i, TlCorner[1] + top_bound))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            top_bound += spcenter
+
         elif current_direction == 1:  # Moving down
             for i in range(top_bound, bottom_bound + 1):
+                if i == bottom_bound:
+                    turn = TlCorner[0] + right_bound, TlCorner[1] + i
                 spirallist.append((TlCorner[0] + right_bound, TlCorner[1] + i))
-                count += 1
-            right_bound -= 1
+            
+            # Go to the bottom edge and back
+            for i in range(bottom_bound + 1, (Static_bbound) + 1):
+                spirallist.append((TlCorner[0] + right_bound, TlCorner[1] + i))
+                backup.append((TlCorner[0] + right_bound, TlCorner[1] + i))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            right_bound -= spcenter
+
         elif current_direction == 2:  # Moving left
             for i in range(right_bound, left_bound - 1, -1):
+                if i == left_bound:
+                    turn = TlCorner[0] + i, TlCorner[1] + bottom_bound
                 spirallist.append((TlCorner[0] + i, TlCorner[1] + bottom_bound))
-                count += 1
-            bottom_bound -= 1
+            
+            # Go to the left edge and back
+            for i in range(left_bound - 1, Static_lbound - 1, -1):
+                spirallist.append((TlCorner[0] + i, TlCorner[1] + bottom_bound))
+                backup.append((TlCorner[0] + i, TlCorner[1] + bottom_bound))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            bottom_bound -= spcenter
+
         elif current_direction == 3:  # Moving up
             for i in range(bottom_bound, top_bound - 1, -1):
+                if i == top_bound:
+                    turn = TlCorner[0] + left_bound, TlCorner[1] + i
                 spirallist.append((TlCorner[0] + left_bound, TlCorner[1] + i))
-                count += 1
-            left_bound += 1
-        
+            
+            # Go to the top edge and back
+            for i in range(top_bound - 1, Static_tbound - 1, -1):
+                spirallist.append((TlCorner[0] + left_bound, TlCorner[1] + i))
+                backup.append((TlCorner[0] + left_bound, TlCorner[1] + i))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            left_bound += spcenter
+
         # Update direction
         current_direction = (current_direction + 1) % 4
-    
+
     return spirallist
 
-waypoints = generate_waypoints((5, 10))
-width = get_width((0, 0), (5, 10), 4)
-height = get_height((0, 0), (5, 10), 4)
-splist = spiral((0, 0), (5, 10), width, height)
-print(splist)
+"""A function to graph the spiral"""
+def graph_spiral(points):
 
+    # Unzip the points into x and y coordinates
+    x, y = zip(*points)
+
+    # Create the plot
+    plt.figure(figsize=(8, 8))
+    plt.plot(x, y, marker='o', markersize=3, linestyle='-')
+    plt.title('Spiral Graph')
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.axis('equal')  # Keep aspect ratio square
+    plt.grid(True)     # Add grid for better visibility
+    plt.show()
+
+first_corner = FIRST_CORNER 
+second_corner=SECOND_CORNER
+snowplow_width = SNOWPLOW_WIDTH
+waypoints = generate_waypoints((10, 20))
+width = get_width(first_corner, second_corner)
+height = get_height(first_corner, second_corner)
+spcenter = get_spcenter(snowplow_width)
+splist = spiral(first_corner, width, height, spcenter)
+print("spiral = ", splist)
+graph_spiral(splist)
 
 
--- project/scripts/waypoint_generation.py
+++ project/scripts/waypoint_generation.py
@@ -9,7 +9,8 @@ import csv # used for writing the waypoints file
 
 
 FIRST_CORNER = (0, 0)
-SECOND_CORNER = (100, 200)
+SECOND_CORNER = (10, 20)
+SNOWPLOW_WIDTH = 4
 
 """Gemerates waypoints based on a rectangle defined by its corners."""
 def generate_waypoints(first_corner, second_corner):
@@ -18,7 +19,6 @@ def generate_waypoints(first_corner, second_corner):
         for j in range(second_corner[1] + 1): # plus one so we get 0 to the very back of the rectangle
             waypoints.append((i, j))
     return waypoints
-
 
 """finds the exact width of the rectangle"""
 def get_width(first_corner, second_corner):
@@ -30,45 +30,104 @@ def get_height(first_corner, second_corner):
     height = second_corner[1] - first_corner[1]
     return(round(height))
 
-"""function to build the spiral"""
-def spiral(TlCorner, rectwidth, rectheight):
-    spirallist = [] # list to hold every point in the spiral
-    current_direction = 0  # start moving right
+"""finds center of snowplow"""
+def get_spcenter(snowplow_width):
+    spcenter = snowplow_width / 2
+    return round(spcenter)
 
-    left_bound = 0 # used to see how far left the spiral can go 
-    right_bound = rectwidth # used to see how far right the spiral can go 
-    top_bound = 0 # used to see how far up the spiral can go 
-    bottom_bound = rectheight # used to see how far down the spiral can go 
-    count = 0  # Count how many steps are taken
+"""function to build the spiral"""
+def spiral(TlCorner, rectwidth, rectheight, spcenter):
+    spirallist = []  # List to hold every point in the spiral
+    current_direction = 0  # Start moving right
+
+    left_bound = 0 + spcenter  # Used to see how far left the spiral can go
+    right_bound = rectwidth - spcenter  # Used to see how far right the spiral can go
+    top_bound = 0 + spcenter  # Used to see how far up the spiral can go
+    bottom_bound = rectheight - spcenter  # Used to see how far down the spiral can go
+    
+    # All static values are used for moving to the edge of the spiral 
+    Static_lbound = 0
+    Static_rbound = rectwidth
+    Static_tbound = 0
+    Static_bbound = rectheight
+
+    for i in range(0, left_bound): # adds the extra points to get into the spiral
+        print("startingpath")
+        spirallist.append((left_bound, i))
 
     while left_bound <= right_bound and top_bound <= bottom_bound:
+        backup = []
         if current_direction == 0:  # Moving right
             for i in range(left_bound, right_bound + 1):
+                if i == right_bound:
+                    turn = (TlCorner[0] + i, TlCorner[1] + top_bound)
                 spirallist.append((TlCorner[0] + i, TlCorner[1] + top_bound))
-                count += 1
-            top_bound += 1
-        
+            
+            # Go to the right edge and back
+            for i in range(right_bound + 1, Static_rbound + 1):
+                spirallist.append((TlCorner[0] + i, TlCorner[1] + top_bound))
+                backup.append((TlCorner[0] + i, TlCorner[1] + top_bound))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            top_bound += spcenter
+
         elif current_direction == 1:  # Moving down
             for i in range(top_bound, bottom_bound + 1):
+                if i == bottom_bound:
+                    turn = TlCorner[0] + right_bound, TlCorner[1] + i
                 spirallist.append((TlCorner[0] + right_bound, TlCorner[1] + i))
-                count += 1
-            right_bound -= 1
-        
+            
+            # Go to the bottom edge and back
+            for i in range(bottom_bound + 1, (Static_bbound) + 1):
+                spirallist.append((TlCorner[0] + right_bound, TlCorner[1] + i))
+                backup.append((TlCorner[0] + right_bound, TlCorner[1] + i))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            right_bound -= spcenter
+
         elif current_direction == 2:  # Moving left
             for i in range(right_bound, left_bound - 1, -1):
+                if i == left_bound:
+                    turn = TlCorner[0] + i, TlCorner[1] + bottom_bound
                 spirallist.append((TlCorner[0] + i, TlCorner[1] + bottom_bound))
-                count += 1
-            bottom_bound -= 1
-        
+            
+            # Go to the left edge and back
+            for i in range(left_bound - 1, Static_lbound - 1, -1):
+                spirallist.append((TlCorner[0] + i, TlCorner[1] + bottom_bound))
+                backup.append((TlCorner[0] + i, TlCorner[1] + bottom_bound))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            bottom_bound -= spcenter
+
         elif current_direction == 3:  # Moving up
             for i in range(bottom_bound, top_bound - 1, -1):
+                if i == top_bound:
+                    turn = TlCorner[0] + left_bound, TlCorner[1] + i
                 spirallist.append((TlCorner[0] + left_bound, TlCorner[1] + i))
-                count += 1
-            left_bound += 1
-        
+            
+            # Go to the top edge and back
+            for i in range(top_bound - 1, Static_tbound - 1, -1):
+                spirallist.append((TlCorner[0] + left_bound, TlCorner[1] + i))
+                backup.append((TlCorner[0] + left_bound, TlCorner[1] + i))
+            
+            for i in range(len(backup) - 1, -1, -1):
+                spirallist.append(backup[i])
+            
+            spirallist.append(turn)
+            left_bound += spcenter
+
         # Update direction
         current_direction = (current_direction + 1) % 4
-    
+
     return spirallist
 
 def waypoints_writer(path):
@@ -81,10 +140,13 @@ def waypoints_writer(path):
 
 if __name__ == "__main__":
     first_corner = FIRST_CORNER 
-    second_corner=SECOND_CORNER
+    second_corner = SECOND_CORNER
+    snowplow_width = SNOWPLOW_WIDTH
     waypoints = generate_waypoints(first_corner, second_corner)
     width = get_width(first_corner, second_corner)
     height = get_height(first_corner, second_corner)
-    path = spiral(first_corner, width, height)
+    spcenter = get_spcenter(snowplow_width)
+    path = spiral(first_corner, width, height, spcenter)
     waypoints_writer(path)
+    print(path)
 
