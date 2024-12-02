00cc2dfcdb052a0cc9e87e554ecd586b89fa9c06
Sebastian Smiley <sebastianswms@gmail.com>
2024-12-01 22:09:49 -0500

    Works :) feature-complete

--- project/snowplow_ws/map_setup.py
+++ project/snowplow_ws/map_setup.py
@@ -1,6 +1,33 @@
 import carla
-client = carla.Client('localhost', 2000)
-world = client.get_world()
-world.unload_map_layer(carla.MapLayer.ParkedVehicles)
-world.unload_map_layer(carla.MapLayer.StreetLights)
-world.unload_map_layer(carla.MapLayer.Walls)
+
+import rclpy
+from rclpy.node import Node
+
+class SimpleController(Node):
+
+    def __init__(self):
+        super().__init__('simple_controller')
+
+        self.client = carla.Client('localhost', 2000)
+        self.client.set_timeout(2.0)
+        self.world = self.client.get_world()
+        self.map = self.world.get_map()
+        self.world.unload_map_layer(carla.MapLayer.ParkedVehicles)
+        self.world.unload_map_layer(carla.MapLayer.StreetLights)
+        self.world.unload_map_layer(carla.MapLayer.Walls)
+
+def main(args=None):
+    """
+    Main function to run the node.
+    """
+    rclpy.init(args=args)
+    node = SimpleController()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
